35821ecb250639d50a08b6284e8444f270338a51
Alejandro Perez Naitoh <alejandropereznaitoh@APN-MBP.local>
2024-11-25 07:33:44 -0600

    Fix createBookCard function. Create createLibrary and addBookToLibrary functions. Fix execution block. Position and style book card grid and cards. deleteBook function missing.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// ---------------Variables and Objects---------------
-
+// ---------------Variables---------------
 const libraryBody = document.getElementById('library-body');
 const addBookButton = document.getElementById('add-book-button');
 const addBookModal = document.getElementById('add-book-modal');
@@ -7,9 +6,9 @@ const bookForm = document.getElementById('book-form');
 const cancelButton = document.getElementById('cancel-button');
 
 const myBooks = [
-    {title: 'To Kill A Mockingbird', author: 'Harper Lee', year: 1960, status: 'read'},
-    {title: '1984', author: 'George Orwell', year: 1949, status: 'not read'},
-    {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925, status: 'reading'},
+    { title: 'To Kill A Mockingbird', author: 'Harper Lee', year: 1960, status: 'read' },
+    { title: '1984', author: 'George Orwell', year: 1949, status: 'not read' },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925, status: 'reading' },
 ];
 
 // ---------------Functions---------------
@@ -21,35 +20,63 @@ function Book(title, author, year, status) {
     this.status = status;
 }
 
-function createBookCard() {         
-    const card = document.createElement("div");
-    const title = document.createElement("h2");
-    const author = document.createElement("h3");
-    const year = document.createElement("p");
-    const status = document.createElement("p");
+function createBookCard(book) {
+    const card = document.createElement('div');
+    const title = document.createElement('h2');
+    const author = document.createElement('h3');
+    const year = document.createElement('p');
+    const status = document.createElement('p');
 
-    card.className = "book-card";
-    title.className = "book-title";
-    author.className = "book-author";
-    year.className = "book-year";
-    status.className = "book-status";
+    card.className = 'book-card';
+    title.className = 'book-title';
+    author.className = 'book-author';
+    year.className = 'book-year';
+    status.className = 'book-status';
 
-    card.appendChild(status);
+    title.textContent = `${book.title}`;
+    author.textContent = `${book.author}`;
+    year.textContent = `${book.year}`;
+    status.textContent = `${book.status}`;
+
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(year);
+    card.appendChild(status);
 
-    libraryBody.appendChild(card);
+    return card;
+}
+
+function createLibrary() {
+    const bookCardContainer = document.createElement('div');
+    bookCardContainer.id = 'book-card-container';
+    bookCardContainer.style.display = 'grid';
+    bookCardContainer.style.gap = '1rem';
+    bookCardContainer.style.gridTemplateColumns = 'repeat(auto-fill, minmax(300px, 1fr))';
+
+    myBooks.forEach((book) => {
+        const card = createBookCard(book);
+        bookCardContainer.appendChild(card);
+    });
+
+    libraryBody.appendChild(bookCardContainer);
+}
+
+function addBookToLibrary(book) {
+    const bookCardContainer = document.getElementById('book-card-container');
+    const newBookCard = createBookCard(book);
+    bookCardContainer.appendChild(newBookCard);
 }
 
 // ---------------Execution---------------
 
+createLibrary();
+
 addBookButton.addEventListener('click', () => addBookModal.showModal());
 cancelButton.addEventListener('click', () => addBookModal.close());
 
-bookForm.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
     const title = document.getElementById('title-input').value;
     const author = document.getElementById('author-input').value;
     const year = document.getElementById('year-input').value;
@@ -58,18 +85,8 @@ bookForm.addEventListener('submit', function(event) {
     const newBook = new Book(title, author, year, status);
 
     myBooks.push(newBook);
+    addBookToLibrary(newBook);
 
-    console.log(books);
-
-    document.getElementById('book-form').reset();
+    bookForm.reset();
+    addBookModal.close();
 });
-
-createBookCard()   
-
-// ---------------Plan---------------
-
-
-// 1. createBookCard - creates book card containing empty elements and appends it into libraryBody element. 
-// 2. addNewBook - modal input it fed into a constructor function that creates a new book object and adds it to the library array. 
-// 3. deleteBook - deletes book card from DOM and book object from the array. 
-// 4. createLibrary - creates individual book cards for every book object in the array.
